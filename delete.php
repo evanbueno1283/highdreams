5dd4881c0f0d787735059d327c44c4b25b54d839
evanbueno1283 <evanbueno1283@gmail.com>
2025-11-05 19:29:07 +0800

    Update database connection details in delete.php

--- delete.php
+++ delete.php
@@ -1,10 +1,10 @@
 <?php
-$host = 'localhost';
-$db = 'fitscan_database';
-$user = 'root';
-$pass = '';
+$host = "mysql-highdreams.alwaysdata.net";
+$user = "439165";
+$pass = "Skyworth23";
+$dbname = "highdreams_1";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
